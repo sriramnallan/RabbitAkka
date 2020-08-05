3d5544b03310de7e3cdb8b25e751da150c7b08c3
sriram nallan <sriramnallan@gmail.com>
2020-08-04 21:14:16 -0400

    first commit to github

---
+++ Consumer/Program.cs
@@ -0,0 +1,97 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using Akka.Actor;
+using Akka.Routing;
+using System.Threading;
+using Topshelf;
+using Timer = System.Timers.Timer;
+using System.Timers;
+using System.Configuration;
+
+namespace Consumer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            if (ConfigurationManager.AppSettings.Get("RunAsService") == "1")
+            {
+                HostFactory.Run(x =>
+                {
+                    x.Service<Consumer>(s =>
+                    {
+                        s.ConstructUsing(name => new Consumer());
+                        s.WhenStarted(tc => tc.StartApp());
+                        s.WhenStopped(tc => tc.StopApp());
+                    });
+                    x.RunAsLocalSystem();
+                    x.SetDescription("Conusmer Host");
+                    x.SetDisplayName("Conusmer Host");
+                    x.SetServiceName("Conusmer");
+                    x.StartAutomatically();
+                });
+            }
+            else
+            {
+                new Consumer().StartApp();
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Consumer
+    {
+        private Timer _timer;
+
+        public Consumer()
+        {
+        }
+
+        public void StartApp()
+        {
+            _timer = new Timer();
+            _timer.Elapsed += ConsumeMessages_Consumer;
+            _timer.Interval = 5000;
+            _timer.Enabled = true;
+        }
+
+        private void ConsumeMessages_Consumer(object source, ElapsedEventArgs e)
+        {
+            _timer.Stop();
+            ActorSystem MedData = ActorSystem.Create("RabbitAkka");
+            Props BaseActor = Props.Create<ConsumerBaseActor>().WithRouter(new RoundRobinPool(200));
+            IActorRef medActor = MedData.ActorOf(BaseActor);
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "RabbitAkka", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    medActor.Tell(body);
+                };
+                channel.BasicConsume(queue: "RabbitAkka", autoAck: true, consumer: consumer);
+                Thread.Sleep(Timeout.Infinite);
+            }
+            
+        }
+
+        public void StopApp()
+        {
+        }
+    }
+
+    public class ConsumerBaseActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            string str = System.Text.Encoding.ASCII.GetString((byte[])message);
+            Console.WriteLine(str);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,80 @@
+﻿using RabbitMQ.Client;
+using System;
+using Topshelf;
+using Timer = System.Timers.Timer;
+using System.Timers;
+using System.Threading;
+using System.Configuration;
+
+namespace Publisher
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            if (ConfigurationManager.AppSettings.Get("RunAsService") == "1")
+            {
+                HostFactory.Run(x =>
+                {
+                   x.Service<Publisher>(s =>
+                    {
+                        s.ConstructUsing(name => new Publisher());
+                        s.WhenStarted(tc => tc.StartApp());
+                        s.WhenStopped(tc => tc.StopApp());
+                    });
+                    x.RunAsLocalSystem();
+                    x.SetDescription("Publisher Host");
+                    x.SetDisplayName("Publisher Host");
+                    x.SetServiceName("Publisher");
+                    x.StartAutomatically();
+                });
+            }
+            else
+            {
+                new Publisher().StartApp();
+                Console.ReadLine();
+            }
+        }
+    }
+    public class Publisher
+    {
+        private Timer _timer;
+        public Publisher()
+        {
+        }
+
+        public void StartApp()
+        {
+            _timer = new Timer();
+            _timer.Elapsed += QueueMessages_Producer;
+            _timer.Interval = 2000;
+            _timer.Enabled = true;
+        }
+
+        private void QueueMessages_Producer(object source, ElapsedEventArgs e)
+        {
+            _timer.Stop();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "RabbitAkka", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var props = channel.CreateBasicProperties();
+            props.ContentType = "text/plain";
+            props.DeliveryMode = 2;
+            int i;
+            for (i = 1; i < 101; i++)
+            {
+                string message = "Message number " + i + " is " + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString();
+                channel.BasicPublish(exchange: "", routingKey: "RabbitAkka", basicProperties: props, body: System.Text.Encoding.ASCII.GetBytes(message));
+            }
+            Console.WriteLine(i);
+            Thread.Sleep(10000);
+            _timer.Start();
+        }
+
+        public void StopApp()
+        {
+            _timer.Stop();
+        }
+    }
+}
